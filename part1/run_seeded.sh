e5d5c3b0441364fe5059d8fa0309df39319fef11
wizard <valentin.schneeberger@epfl.ch>
2025-05-01 13:30:49 +0200

    Add script for part1 and observed coverage

---
+++ part1/run_seeded.sh
@@ -0,0 +1,4 @@
+python3 ../infra/helper.py build_fuzzers libxml2
+mkdir ../build/out/part1Seed
+cp -av ../seeds/xml/* ../build/out/part1Seed/
+python3 ../infra/helper.py run_fuzzer libxml2 xml --corpus-dir=build/out/part1Seed
---
+++ part1/run_unseeded.sh
@@ -0,0 +1,3 @@
+python3 ../infra/helper.py build_fuzzers libxml2
+mkdir ../build/out/part1NoSeed
+python3 ../infra/helper.py run_fuzzer libxml2 xml --corpus-dir=build/out/part1NoSeed
