d61a6cf56af8fe691360d6552636c7998505a09d
wizard <valentin.schneeberger@epfl.ch>
2025-05-01 14:35:20 +0200

    Create build and out in the case they're not here

--- part1/run_seeded.sh
+++ part1/run_seeded.sh
@@ -1,4 +1,6 @@
 python3 ../infra/helper.py build_fuzzers libxml2
+mkdir ../build
+mkdir ../build/out
 mkdir ../build/out/part1Seed
 cp -av ./seeds/xml/* ../build/out/part1Seed/
 python3 ../infra/helper.py run_fuzzer libxml2 xml --corpus-dir=build/out/part1Seed
--- part1/run_unseeded.sh
+++ part1/run_unseeded.sh
@@ -1,3 +1,5 @@
 python3 ../infra/helper.py build_fuzzers libxml2
+mkdir ../build
+mkdir ../build/out
 mkdir ../build/out/part1NoSeed
 python3 ../infra/helper.py run_fuzzer libxml2 xml --corpus-dir=build/out/part1NoSeed
