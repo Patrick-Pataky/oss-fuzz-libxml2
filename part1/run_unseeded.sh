02fa75e1bb7d4dd11ae4f8df70af494cbf0450c6
wizard <valentin.schneeberger@epfl.ch>
2025-05-06 12:27:16 +0200

    Update seed and unseed script

--- part1/run_unseeded.sh
+++ part1/run_unseeded.sh
@@ -2,4 +2,5 @@ python3 ../infra/helper.py build_fuzzers libxml2
 mkdir ../build
 mkdir ../build/out
 mkdir ../build/out/part1NoSeed
+rm -rf ../build/out/liblxm2/xml_seed_corpus.zip
 python3 ../infra/helper.py run_fuzzer libxml2 xml --corpus-dir=build/out/part1NoSeed
