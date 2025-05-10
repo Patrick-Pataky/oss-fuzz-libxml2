65e2b845b47b36fe230de7b88193d7cd18917c02
Patrick-Pataky <patrick.pataky@epfl.ch>
2025-05-10 17:38:57 +0200

    Added debug with gdb instructions

--- part3/reproduce.sh
+++ part3/reproduce.sh
@@ -4,6 +4,12 @@ set -e
 
 cd ..
 
-python3 infra/helper.py build_fuzzers libxml2
+python3 infra/helper.py build_fuzzers libxml2 #--sanitizer none
 python infra/helper.py reproduce libxml2 catalog \
-    build/out/libxml2/leak-c67be65e77fca0912ba00e113cd81dcb12eebd9a
+    build/out/libxml2/leak-c67be65e77fca0912ba00e113cd81dcb12eebd9a \
+    -e ASAN_OPTIONS="detect_leaks=0" \
+    -e LSAN_OPTIONS="detect_leaks=0"
+
+# Debug using gdb:
+# python ../infra/helper.py shell base-runner-debug
+# gdb --args /out/libxml2/catalog /out/libxml2/leak-c67be65e77fca0912ba00e113cd81dcb12eebd9a
