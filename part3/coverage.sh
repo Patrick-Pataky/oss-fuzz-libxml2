14cccda4b74e5a249ddeaaa1e782916b9b09cd05
Patrick-Pataky <patrick.pataky@epfl.ch>
2025-05-15 17:25:31 +0200

    Removed useless files

--- part3/coverage.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-cd ..
-
-python3 infra/helper.py build_fuzzers libxml2 --sanitizer coverage
-python3 infra/helper.py coverage libxml2 --fuzz-target $1 --corpus-dir build/out/$1
