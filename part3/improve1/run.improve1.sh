de96b51b68ec3ed6003b5c853be2f81e73d8982e
Patrick-Pataky <patrick.pataky@epfl.ch>
2025-05-15 14:50:35 +0200

    Completed run.improve1.sh

---
+++ part3/improve1/run.improve1.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Usage: run.improve1.sh
+
+set -e
+
+../patch.sh reset
+../patch.sh apply catalog
+
+HARNESS="catalog"
+
+P1="build/1"
+P2="build/2"
+P3="build/3"
+
+mkdir -p $P1
+mkdir -p $P2
+mkdir -p $P3
+
+# Run the harness 3 times for 4 hours each
+python3 ../../infra/helper.py build_fuzzers libxml2
+
+python3 ../../infra/helper.py run_fuzzer --corpus-dir $P1 --sanitizer none \
+    libxml2 $HARNESS -- -max_total_time=14400 -fork=1
+python3 ../../infra/helper.py run_fuzzer --corpus-dir $P2 --sanitizer none \
+    libxml2 $HARNESS -- -max_total_time=14400 -fork=1
+python3 ../../infra/helper.py run_fuzzer --corpus-dir $P3 --sanitizer none \
+    libxml2 $HARNESS -- -max_total_time=14400 -fork=1
+
+# Merge the corpus and run coverage
+cp -r $P2/* $P1
+cp -r $P3/* $P1
+
+rm -rf $P2
+rm -rf $P3
+
+python3 infra/helper.py build_fuzzers libxml2 --sanitizer coverage
+
+COV_PATH="coverage_improve1"
+rm -rf $COV_PATH
+mkdir -p $COV_PATH
+
+python3 infra/helper.py coverage libxml2 --fuzz-target $1 --corpus-dir $COV_PATH
+
+echo "Done!"
